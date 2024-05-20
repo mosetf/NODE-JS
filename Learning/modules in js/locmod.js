9e9c948a7cfc745ff497ab1d98d54dbd083838b7
mosetf <stanlym315@gmail.com>
2024-05-20 15:05:12 +0300

    Add new module for greeting in locmod.js

---
+++ Learning/modules in js/locmod.js
@@ -0,0 +1,10 @@
+const welcome ={
+    sayhello: function() {
+        console.log("Hello stan");
+    },
+    currTime: new Date().toLocaleDateString(),
+    companyName: "stan`s"
+
+}
+
+module.exports = welcome
