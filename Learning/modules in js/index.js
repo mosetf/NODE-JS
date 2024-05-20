b459df3bfca5e72af5258423938c847103e1b4f1
mosetf <stanlym315@gmail.com>
2024-05-20 16:26:08 +0300

    chore: Refactor code for error handling using assert module

--- Learning/modules in js/index.js
+++ Learning/modules in js/index.js
@@ -1,9 +1,24 @@
 console.clear();
 const assert = require('assert').strict;
-
+/*
 try {
     assert.deepStrictEqual({a:'5'}, {a:'5'});
     console.log('No error occured');
 } catch(error){
     console.log('Error:', error)
 }
+*/
+
+//assert.doesnotthrow()
+ try {
+    assert.doesNotThrow(
+        () => {
+            throw new TypeError(' the wrong value error');
+        },
+        /abcd/,
+        'Whoops'
+    );
+ } catch(error){
+    console.log("error", error);
+ }
+
